fb9df2eb940742c53c472611e960be281f1ff03b
chertik77 <denysbabych.dev@gmail.com>
2024-07-11 23:01:26 +0100

    chore: set up all config files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,7 @@
 import type { Config } from 'tailwindcss'
 
-const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
-  ],
+export default {
+  content: ['./src/**/*.{ts,tsx}'],
   theme: {
     extend: {
       backgroundImage: {
@@ -16,5 +12,4 @@ const config: Config = {
     }
   },
   plugins: []
-}
-export default config
+} satisfies Config
